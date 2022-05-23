24e36bd197a3aa64f2c4a3bba03aefaf4daaf8aa
recepenes <turktenacious@gmail.com>
2022-05-23 23:02:29 +0300

    Add a default host builder Setup the default route to access a resource

--- ConsoltoWebAPI/Program.cs
+++ ConsoltoWebAPI/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context =>
+
+     {
+         await context.Response.WriteAsync("Hello from new Web API app");
+     });
+    endpoints.MapGet("/test", async context =>
+
+     {
+         await context.Response.WriteAsync("Hello from new Web API app test");
+     });
+});
+
+app.UseStaticFiles();
+
+app.Run();
