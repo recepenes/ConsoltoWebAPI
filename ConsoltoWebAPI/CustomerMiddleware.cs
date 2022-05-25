26c707f88b44206a1795a24f24b45e1d13416d40
recepenes <turktenacious@gmail.com>
2022-05-25 19:28:35 +0300

    Create Custom Middleware in a separate file

---
+++ ConsoltoWebAPI/CustomerMiddleware.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoletoWebAPI
+{
+    public class CustomerMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("Hello from new file 1 \n");
+
+            await next(context);
+
+            await context.Response.WriteAsync("Hello from new file 2 \n");
+        }
+    }
+}
--- ConsoltoWebAPI/Program.cs
+++ ConsoltoWebAPI/Program.cs
@@ -1,19 +1,21 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using ConsoletoWebAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddTransient<CustomerMiddleware>();
+
 var app = builder.Build();
+
+app.UseMiddleware<CustomerMiddleware>();
+
+app.Run(context =>
+ context.Response.WriteAsync("Hello from Use-1 1\n"));
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-}
-else
-{
-
-
 }
 
 app.UseRouting();
