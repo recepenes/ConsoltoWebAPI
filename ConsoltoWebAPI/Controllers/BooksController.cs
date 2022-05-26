cd0158f18c367f4622d261cc736e9811129ff24d
recepenes <turktenacious@gmail.com>
2022-05-26 20:02:21 +0300

    Route Constraints

---
+++ ConsoltoWebAPI/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoletoWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [Route("{id:int:min(10):max(100)}")]
+        public string GetById(int id)
+        {
+            return $"hello int  {id}";
+        }
+
+        [Route("{id:length(5):alpha}")]
+        public string GetByIdString(string id)
+        {
+            return $"hello string {id}";
+        }
+
+        [Route("{id:regex(a(b|c))}")]
+        public string GetByIdRegex(string id)
+        {
+            return $"hello regex {id}";
+        }
+    }
+}
